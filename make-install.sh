663812040caaf4933cf2c65b949dd7efb1546980
Marek Prochera <marek.prochera@gmail.com>
2014-01-02 20:01:16 +0000

    init

---
+++ make-install.sh
@@ -0,0 +1,11 @@
+ipkg update 
+ipkg upgrade 
+ipkg install git 
+ipkg install textutils	 
+ipkg install python27	
+ipkg install py27-setuptools	 
+ipkg install mlocate
+
+
+
+cd /tmp/subliminal /opt/bin/python2.7 setup.py install 
