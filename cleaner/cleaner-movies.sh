6d70eb9e22590419b81090ea15f43880680d3de5
Marek Prochera <marek.prochera@gmail.com>
2014-01-03 23:18:29 +0000

    cleaner

---
+++ cleaner/cleaner-movies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+in="/share/Download/transmission/completed" 
+out="/share/Movies/movies-new"
+
+FILE_TYPES="-name \"*.mkv\" -o -name \"*.avi\" "
+
+#  for file in $(/opt/bin/find -L . \( ${FILE_TYPES} \))
+
+
+search(){
+  dir=$1
+  type=$2
+  echo "Find in "$dir  
+  for file in $(find $dir -type f -name "*.$type")
+  do
+
+    echo File: $file
+
+  done
+}
+echo init
+search $in $FILE_TYPES
